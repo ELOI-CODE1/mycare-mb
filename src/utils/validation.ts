043539f550f127ba54293419f05d7b0c021ec420
ELOI-CODE1 <izabayoeloi2@gmail.com>
2026-07-22 14:56:55 +0200

    feat(validation):add validations.ts for checking user creating account

---
+++ src/utils/validation.ts
@@ -0,0 +1,65 @@
+export interface SignupFormData {
+    fullName: string
+    email: string 
+    password: string
+    phone: string
+}
+
+export interface AnswersData {
+    q1?: string
+    q2?: string
+}
+
+export interface FormErrors {
+    fullName?: string
+    email?: string
+    password?: string 
+    phone?: string
+    q1?: string
+    q2?: string
+}
+
+//email regex
+const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+//phone regex
+const PHONE_REGEX = /^(?:\+250|250|0)?7[2389]\d{7}$/;
+
+export const validateStepOne = (data: SignupFormData): { isValid: boolean; errors: FormErrors}  => {
+    const errors: FormErrors = {}
+
+    //name
+    if (!data.fullName.trim()) {
+        errors.fullName = 'Full name is required.'
+    } else if (data.fullName.trim().length < 3) {
+        errors.fullName = 'Name must be at least 3 characters long.'
+    }
+
+    //email
+    if (!data.email.trim()) {
+        errors.email = 'Email address is required.'
+    } else if (!EMAIL_REGEX.test(data.email.trim())) {
+        errors.email = 'Please enter a valid email address.'
+    }
+
+    //password
+    if (!data.password) {
+        errors.password = 'Password is required.'
+    } else if (data.password.length < 6) {
+        errors.password = 'Password must be at least 6 characters long.'
+    }
+
+    //Phone number
+    if (!data.phone.trim()) {
+        errors.phone = 'Phone number is required.'
+    } else if (!PHONE_REGEX.test(data.phone.trim().replace(/\s+/g, ''))) {
+        errors.phone = 'Enter a valid phone number.'
+    }
+
+    return{
+        isValid: Object.keys(errors).length === 0,
+        errors,
+    }
+
+}
+
