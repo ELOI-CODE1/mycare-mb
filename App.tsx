4d52d302d30c34c995dc260f37e0d80bf87e71e0
ELOI-CODE1 <izabayoeloi2@gmail.com>
2026-09-07 15:18:16 +0200

    update web

--- App.tsx
+++ App.tsx
@@ -59,7 +59,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   splashOverlay: {
-    ...StyleSheet.absoluteFillObject,
+    ...StyleSheet.absoluteFill,
     backgroundColor: '#E6F4FE',
     justifyContent: 'center',
     alignItems: 'center',
